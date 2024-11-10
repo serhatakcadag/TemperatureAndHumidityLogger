c2c6ccdf8e7c475ddea8052d4c3ad80f48658ca3
serhatakcadag <serhat.akcadag@gmail.com>
2024-11-10 15:08:39 +0300

    Boilerplate stuff and layers has added.

---
+++ backend/TemperatureAndHumidityLogger.Backend/TemperatureAndHumidityLogger.WebApi/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace TemperatureAndHumidityLogger.WebApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers(); // Maps attribute-routed controllers.
+            });
+        }
+    }
+}
