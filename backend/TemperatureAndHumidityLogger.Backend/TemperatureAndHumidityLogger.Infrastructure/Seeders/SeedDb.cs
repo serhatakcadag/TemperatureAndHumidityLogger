b7bc86cc2fca2cb055e3670bd51223853f719ff3
serhatakcadag <serhat.akcadag@gmail.com>
2024-12-08 20:01:44 +0300

    Creating ui app

--- backend/TemperatureAndHumidityLogger.Backend/TemperatureAndHumidityLogger.Infrastructure/Seeders/SeedDb.cs
+++ backend/TemperatureAndHumidityLogger.Backend/TemperatureAndHumidityLogger.Infrastructure/Seeders/SeedDb.cs
@@ -10,14 +10,14 @@ namespace TemperatureAndHumidityLogger.Infrastructure.Seeders
     {
         public static async Task SeedDatabaseAsync(this IServiceProvider serviceProvider)
         {
-            await serviceProvider.SeedRolesAsync();
-            await serviceProvider.SeedAdminUserAsync();
-
             using (var scope = serviceProvider.CreateScope())
             {
                 var dbContext = scope.ServiceProvider.GetRequiredService<EfDbContext>();
                 dbContext.Database.Migrate();
             }
+
+            await serviceProvider.SeedRolesAsync();
+            await serviceProvider.SeedAdminUserAsync();
         }
     }
 }
